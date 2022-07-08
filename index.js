9600bbad4200abe8d7978c85e1ff8cc0beb7c502
Sucredoux <sakharovaes@yandex.ru>
2022-07-08 23:17:52 +0200

    Make responsive

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 let profile = document.querySelector('.profile');
-
 let popup = document.querySelector('.pop-up');
 let editProfile = profile.querySelector('.profile__edit-button');
 let closePopup = popup.querySelector('.pop-up__close');
-let popupField = popup.querySelector('.pop-up__field');
-let submitButton = popup.querySelector('.pop-up__submit-button');
-let personName = profile.querySelector('.profile__name');
-let personDescr = profile.querySelector('.profile__description');
-  console.log(personName);
-  console.log(personDescr);
-
 
 editProfile.addEventListener('click', function () {
   popup.classList.add('pop-up_opened');
 }
 );
+
 closePopup.addEventListener('click', function() {
   popup.classList.remove('pop-up_opened');
 })
+
+let personName = profile.querySelector('.profile__name');
+let personDescr = profile.querySelector('.profile__description');
 
 function submitChangeProfile(evt) {
   evt.preventDefault();
@@ -27,7 +23,6 @@ function submitChangeProfile(evt) {
   personDescr.textContent = formDescr;
   popup.classList.remove('pop-up_opened');
 }
-
 
 let formPopup = popup.querySelector('.form');
 formPopup.addEventListener('submit', submitChangeProfile);
